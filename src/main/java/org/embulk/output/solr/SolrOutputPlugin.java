d289c2db4fe98ff99767f098d9fe5241a6449834
吉田　匠 <takumyos@yahoo-corp.jp>
2016-02-17 11:49:32 +0900

    implements methods.

--- src/main/java/org/embulk/output/solr/SolrOutputPlugin.java
+++ src/main/java/org/embulk/output/solr/SolrOutputPlugin.java
@@ -1,77 +1,235 @@
 package org.embulk.output.solr;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
-import com.google.common.base.Optional;
+import com.google.common.base.Throwables;
+import com.google.inject.Inject;
+
 import org.embulk.config.TaskReport;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.common.SolrInputDocument;
 import org.embulk.config.Config;
 import org.embulk.config.ConfigDefault;
 import org.embulk.config.ConfigDiff;
 import org.embulk.config.ConfigSource;
 import org.embulk.config.Task;
 import org.embulk.config.TaskSource;
+import org.embulk.spi.Column;
+import org.embulk.spi.ColumnVisitor;
 import org.embulk.spi.Exec;
 import org.embulk.spi.OutputPlugin;
-import org.embulk.spi.PageOutput;
+import org.embulk.spi.Page;
+import org.embulk.spi.PageReader;
 import org.embulk.spi.Schema;
 import org.embulk.spi.TransactionalPageOutput;
+import org.embulk.spi.time.Timestamp;
+import org.msgpack.value.Value;
+import org.slf4j.Logger;
 
-public class SolrOutputPlugin
-        implements OutputPlugin
-{
-    public interface PluginTask
-            extends Task
-    {
-        // configuration option 1 (required integer)
-        @Config("option1")
-        public int getOption1();
+public class SolrOutputPlugin implements OutputPlugin {
+    public interface PluginTask extends Task {
+        @Config("host")
+        public String getHost();
 
-        // configuration option 2 (optional string, null is not allowed)
-        @Config("option2")
-        @ConfigDefault("\"myvalue\"")
-        public String getOption2();
+        @Config("port")
+        @ConfigDefault("8080")
+        public int getPort();
 
-        // configuration option 3 (optional string, null is allowed)
-        @Config("option3")
-        @ConfigDefault("null")
-        public Optional<String> getOption3();
+        @Config("collection")
+        public String getCollection();
+        
+        @Config("bulkSize")
+        @ConfigDefault("1000")
+        public int getBulkSize();
+    }
+
+    private final Logger logger;
+
+    @Inject
+    public SolrOutputPlugin() {
+        logger = Exec.getLogger(getClass());
     }
 
     @Override
-    public ConfigDiff transaction(ConfigSource config,
-            Schema schema, int taskCount,
-            OutputPlugin.Control control)
-    {
+    public ConfigDiff transaction(ConfigSource config, Schema schema, int taskCount, OutputPlugin.Control control) {
         PluginTask task = config.loadConfig(PluginTask.class);
-
-        // retryable (idempotent) output:
-        // return resume(task.dump(), schema, taskCount, control);
-
-        // non-retryable (non-idempotent) output:
         control.run(task.dump());
         return Exec.newConfigDiff();
     }
 
     @Override
-    public ConfigDiff resume(TaskSource taskSource,
-            Schema schema, int taskCount,
-            OutputPlugin.Control control)
-    {
+    public ConfigDiff resume(TaskSource taskSource, Schema schema, int taskCount, OutputPlugin.Control control) {
         throw new UnsupportedOperationException("solr output plugin does not support resuming");
     }
 
     @Override
-    public void cleanup(TaskSource taskSource,
-            Schema schema, int taskCount,
-            List<TaskReport> successTaskReports)
-    {
+    public void cleanup(TaskSource taskSource, Schema schema, int taskCount, List<TaskReport> successTaskReports) {
     }
 
     @Override
-    public TransactionalPageOutput open(TaskSource taskSource, Schema schema, int taskIndex)
-    {
+    public TransactionalPageOutput open(TaskSource taskSource, Schema schema, int taskIndex) {
         PluginTask task = taskSource.loadTask(PluginTask.class);
+        SolrClient client = createSolrClient(task);
+        TransactionalPageOutput pageOutput = new SolrPageOutput(client, schema, task);
+        return pageOutput;
+    }
 
-        // Write your code here :)
-        throw new UnsupportedOperationException("SolrOutputPlugin.run method is not implemented yet");
+    /**
+     * create Solr Client. <br>
+     * this method has not been supporting SolrCloud client using zookeeper
+     * Host, yet.
+     * 
+     * @param task
+     * @return SolrClient instance.
+     */
+    private SolrClient createSolrClient(PluginTask task) {
+        SolrClient solr = new HttpSolrClient(
+                "http://" + task.getHost() + ":" + task.getPort() + "/solr/" + task.getCollection());
+        return solr;
+    }
+
+    public static class SolrPageOutput implements TransactionalPageOutput {
+
+        private Logger logger;
+        private SolrClient client;
+        private final PageReader pageReader;
+        private final Schema schema;
+        private PluginTask task;
+        private final int bulkSize;
+        
+        List<SolrInputDocument> documentList = new LinkedList<SolrInputDocument>();
+
+        public SolrPageOutput(SolrClient client, Schema schema, PluginTask task) {
+            this.logger = Exec.getLogger(getClass());
+            this.client = client;
+            this.pageReader = new PageReader(schema);
+            this.schema = schema;
+            this.task = task;
+            this.bulkSize = task.getBulkSize();
+        }
+
+        @Override
+        public void add(Page page) {
+            pageReader.setPage(page);
+            while (pageReader.nextRecord()) {
+                final SolrInputDocument doc = new SolrInputDocument();
+                
+                schema.visitColumns(new ColumnVisitor() {
+
+                    @Override
+                    public void booleanColumn(Column column) {
+                        if (pageReader.isNull(column)) {
+                            //do nothing.
+                        } else {
+                            boolean value = pageReader.getBoolean(column);
+                            doc.addField(column.getName(), value);
+                        }
+                    }
+
+                    @Override
+                    public void longColumn(Column column) {
+                        if (pageReader.isNull(column)) {
+                            //do nothing.
+                        } else {
+                            long value = pageReader.getLong(column);
+                            doc.addField(column.getName(), value);
+                        }
+                    }
+
+                    @Override
+                    public void doubleColumn(Column column) {
+                        if (pageReader.isNull(column)) {
+                            //do nothing.
+                        } else {
+                            double value = pageReader.getDouble(column);
+                            doc.addField(column.getName(), value);
+                        }
+                    }
+
+                    @Override
+                    public void stringColumn(Column column) {
+                        if (pageReader.isNull(column)) {
+                            //do nothing.
+                        } else {
+                            String value = pageReader.getString(column);
+                            doc.addField(column.getName(), value);
+                        }
+                    }
+
+                    @Override
+                    public void timestampColumn(Column column) {
+                        if (pageReader.isNull(column)) {
+                            //do nothing.
+                        } else {
+                            Timestamp value = pageReader.getTimestamp(column);
+                            Date dateValue = new Date(value.getEpochSecond());
+                            doc.addField(column.getName(), dateValue);
+                        }
+                    }
+
+                    @Override
+                    public void jsonColumn(Column column) {
+                        if (pageReader.isNull(column)) {
+                            //do nothing.
+                        } else {
+                            Value value = pageReader.getJson(column);
+                            doc.addField(column.getName(), value.toString()); // send json as string.
+                        }
+                    }
+                });
+                
+                System.out.println(doc);
+                
+                documentList.add(doc);
+                
+                if (documentList.size() >= bulkSize) {
+                    try {
+                        client.add(documentList);
+                        client.commit();
+                        documentList.clear();
+                    } catch (SolrServerException | IOException e) {
+                        Throwables.propagate(e); //  TODO error handling
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void finish() {
+            // send rest of all documents.
+            if (!documentList.isEmpty()) {
+                try {
+                    client.add(documentList);
+                    client.commit();
+                    documentList.clear();
+                } catch (SolrServerException | IOException e) {
+                    Throwables.propagate(e); //  TODO error handling
+                }
+            }
+        }
+
+        @Override
+        public void close() {
+            try {
+                client.close();
+                client = null;
+            } catch (IOException e) {
+                Throwables.propagate(e); //  TODO error handling
+            }
+        }
+
+        @Override
+        public void abort() {
+        }
+
+        @Override
+        public TaskReport commit() {
+            TaskReport report = Exec.newTaskReport();
+            return report;
+        }
     }
 }
